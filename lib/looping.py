b15c97766db43ac3d90f1f3f04504c886c8d2420
Lucy <lucybellott@gmail.com>
2023-11-27 15:48:11 -0500

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,52 @@
 #!/usr/bin/env python3
 
+import math
+
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+        
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_list = list()
+    for num in int_list:
+        square = math.pow(num,2)
+        square_list.append(square)
+    return square_list    
+    
+    #option 2 
+    #return [i ** 2 for i in int_list]
 
-def fizzbuzz():
+
+# def fizzbuzz():
     # code goes here!
-    pass
+    # counter = 1
+
+    # while(counter <= 100):
+    #     if(counter % 3 == 0 and counter % 5 == 0):
+    #         print("Fizzbuzz")
+    #     elif(counter % 3== 0):
+    #         print("Fizz")    
+    #     elif(counter % 5 == 0):
+    #         print("Buzz")
+    #     else:
+    #         print(counter)
+    # counter +=1        
+
+
+#0r
+def fizzbuzz():
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
